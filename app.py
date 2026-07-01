0041153368ba302b5f1ab846ea58ba85a3775b10
PaulHan1212 <paulweitinghan@gmail.com>
2026-06-30 22:56:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if uploaded_file is not None:
 
         result["缺少戰功"] = result["應達戰功"] - result["戰功本週"]
         result["需繳資源"] = result["缺少戰功"].apply(
-            lambda x: math.ceil(x / 10000) * 800000
+            lambda x: math.ceil(x / 10000) * 600000
         )
 
         total_resource = result["需繳資源"].sum()
